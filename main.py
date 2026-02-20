4732677b5842b90239526e5d11a7e3ab684d5e10
kiatkriangkraip-png <kiatkriangkrai.p@rmutsvmail.com>
2026-02-20 16:36:46 +0700

    Update add function to return 0 for negative input
    Modify add function to handle negative values by returning 0.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def add(a, b):
-    if a > 0:
-        return a + b  
-    return b
+    if a < 0:
+        return 0  # ถ้า a ติดลบ ให้คืนค่า 0 ทันที
+    return a + b  # ถ้า a ไม่ติดลบ ให้บวกกันปกติ
